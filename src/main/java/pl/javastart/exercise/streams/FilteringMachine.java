2ad5fc59ebbeacbf49192a0b2087cc7458601949
wkos <obohp@wp.pl>
2020-08-04 12:40:48 +0200

    imported source code

---
+++ src/main/java/pl/javastart/exercise/streams/FilteringMachine.java
@@ -0,0 +1,23 @@
+package pl.javastart.exercise.streams;
+
+import java.util.List;
+
+public class FilteringMachine {
+
+    public List<Integer> filterOutNotEvenNumbers(List<Integer> numberList) {
+        return null;
+    }
+
+    public List<Integer> filterOutLowerNumbersThan20(List<Integer> numberList) {
+        return null;
+    }
+
+    public List<Book> convertToBooks(List<String> titles) {
+        return null;
+    }
+
+    // Metoda powinna przekształcić tytuły na książki i zwrócić tylko te które rozpoczynają się od słowa "Gra"
+    public List<Book> convertToBooksAndReturnOnlyStartingWithGra(List<String> titles) {
+        return null;
+    }
+}
---
+++ src/main/java/pl/javastart/exercise/streams/FilteringMachineTwo.java
@@ -0,0 +1,15 @@
+package pl.javastart.exercise.streams;
+
+import java.util.List;
+
+public class FilteringMachineTwo {
+
+    // metoda powinna zwrócić imiona wszystkich niedorosłych osób (mniej niż 18 lat)
+    public List<String> findKidNames(List<Person> ppl) {
+        return null;
+    }
+
+    public List<User> convertPeopleToUsers(List<Person> people) {
+        return null;
+    }
+}
